0d912630a89e33a8ffe6509de00c31bd37874d0f
marwahmh <marwahisham@aucegypt.edu>
2019-06-27 18:07:01 +0200

    updated project

--- binary_number.h
+++ binary_number.h
@@ -1,4 +1,6 @@
 #pragma once
+#include<vector>
+using namespace std;
 class binary_number
 {
 
@@ -9,22 +11,14 @@ public:
 	unsigned num;   //the binary number 
 	unsigned dashes;  // represents the locations of the dashes by ones
 	bool is_used;    //flag that indicates whether it is used in combinations
+	vector<int> covered_mins; //every number has a vector that includes all the minterms it covers 
 	binary_number();
 	~binary_number();
 
 	unsigned count_ones(unsigned num);
 	void print_number(unsigned num);
-	void create_MinToBin();
-	void print_MinToBin();
-	void create_combined();
-	void print_combined();//***
-	void print_withDashes();
-	void create_finalTable();
-	void print_finalTable();
-	void print_finalMinterms();
-	//bool is_printed();
-	//void init(); //start the table making and printing
-//void getinput(); //get input from user
-//unsigned count_bits(unsigned n); //min bits to represent a number
+	int decimal();
+	void push_decimal();
+	
 };
 
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "binary_number.h"
+
+using namespace std;
+
+void create_MinTable();
+void print_MinTable();
+void create_combined();
+void print_combined();//***
+void print_withDashes();
+void create_finalTable();
+void print_finalTable();
+void print_finalMinterms();
+//bool is_printed();
+//void init(); //start the table making and printing
+//void getinput(); //get input from user
+//unsigned count_bits(unsigned n); //min bits to represent a number
+
+int main()
+{
+
+}
