daf8410114b6b791ed94c8a66e29ddd32a4ba9f6
mohammed <mohd.osa@hotmail.com>
2019-06-27 19:29:03 +0200

    recent updates

--- binary_number.cpp
+++ binary_number.cpp
@@ -1,13 +1,74 @@
 #include "binary_number.h"
-
+#include<string>
 
 
 binary_number::binary_number()
 {
     num = 0;
-    
+    dashes=0;
+    is_used=false;
+	covered_mins.resize(0);
 }
 
+binary_number::binary_number(int dec)
+{
+	num = decimal_to_binary(dec);
+	dashes = 0;
+	is_used = false;
+	covered_mins.resize(0);
+}
+
+unsigned binary_number:: count_ones(unsigned num)
+{
+	short bit = 0;
+	int count = 0;
+	while (num > 0) 
+	{
+		bit = num % 2;
+		num >>= 1;
+		if (bit) {
+			count++;
+		}
+	}
+	return count;
+}
+void binary_number::print_number(unsigned num, int Bits_num)
+{
+	vector<unsigned> bits;
+	bits.resize(Bits_num);
+	int count = 0; 
+	while (num > 0 || count < Bits_num)
+	{
+		bits[count] = num % 2;
+		num >>= 1;
+		count++;
+	}
+	for (int i = count - 1; i >= 0; i--)
+		cout << bits[i];
+}
+int binary_number::binary_to_decimal(unsigned bin)
+{
+	string bin_st;
+	bin_st = bin;
+	int dec = stoi(bin_st, nullptr, 2);
+	return dec;
+}
+void binary_number:: push_decimal(int dec)
+{
+	covered_mins.push_back(dec);
+}
+
+unsigned binary_number::decimal_to_binary (int dec)
+{
+	string bin;
+	while (dec > 0)
+	{
+		bin = bin + char(dec % 2);
+		dec >>= 1;
+	}
+	unsigned res = stoi(bin);
+	return res;
+}
 
 binary_number::~binary_number()
 {
--- binary_number.h
+++ binary_number.h
@@ -1,5 +1,7 @@
 #pragma once
 #include<vector>
+#include<iostream>
+#include<string>
 using namespace std;
 class binary_number
 {
@@ -12,13 +14,15 @@ public:
 	unsigned dashes;  // represents the locations of the dashes by ones
 	bool is_used;    //flag that indicates whether it is used in combinations
 	vector<int> covered_mins; //every number has a vector that includes all the minterms it covers 
+	binary_number( int dec );
 	binary_number();
 	~binary_number();
 
-	unsigned count_ones(unsigned num);
-	void print_number(unsigned num);
-	int decimal();
-	void push_decimal();
+	unsigned count_ones(unsigned num);  // counts the number of ones in the binary number
+	void print_number(unsigned num, int Bits_num); // prints the binary number to Bits_num decimal places
+	int binary_to_decimal( unsigned bin ); // converts binary to decimal
+	unsigned decimal_to_binary(int dec); // converts binary to decimal
+	void push_decimal(int dec); // push decimal number to the vector
 	
 };
 
